b4061cecf64cf7f35285affc4882114bfc954766
Abubakri Sherif <75755419+saykeed@users.noreply.github.com>
2022-01-13 01:26:22 +0100

    made the cart page load carted meals
    but im stuck on trying to  make sure that whenever a user clicks the + button it should only add to the quantity of the meal the user clicked, hopefully willl work it out sha

--- js/cart.js
+++ js/cart.js
@@ -1,46 +1,105 @@
 let getCartMealDiv = document.querySelector("#cartDiv");
-let getPlusMealQuantity = document.querySelector("#plus");
-let getMinusMealQuantity = document.querySelector("#minus");
-let getMealQuantityValue = document.querySelector("#value");
-let getMealPrice = document.querySelector("#priceAmount span")
-let defaultMealPrice = 1000;
-
-getMealQuantityValue.innerHTML = 1;
-getMealPrice.innerHTML = defaultMealPrice;
 
 
 
 // getting list of meals id added to cart
 let getCartedMealIdFromLS = function () {
     const cartMealsId = JSON.parse(localStorage.getItem('cartedMealId'));
-    console.log(cartMealsId);
     getCartedMeal(cartMealsId);
 }
 
 
 
 // fetching carted meal data section
-let getCartedMeal =  async function (id) {
+let getCartedMeal = async function (id) {
     if (id == null) {
         getCartMealDiv.innerHTML = "Your haven't add any meals to your cart"
     } else {
         getCartMealDiv.innerHTML = "";
     }
+
     
-    for (let i = id.length-1; i >= 0; i--) {
+    for (let i = 0; i < id.length; i++) {
         let CartmealUrl = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + id[i];
         let Cartmeal = await fetch(CartmealUrl);
         let CartmealRes  = await Cartmeal.json();
         CartmealData = CartmealRes.meals[0];
-        getCartMealDiv.innerHTML += id[i]
+        getCartMealDiv.innerHTML += `
+            <div id="mealcart">
+                <label for="${CartmealData.idMeal}" id="checkLabel">
+                    <div id="checkDiv">
+                        <input type="checkbox" id="${CartmealData.idMeal}">
+                        <div class="customCheckbox"></div>
+                    </div>
+                    <img src="${CartmealData.strMealThumb}" alt="">
+                </label>
+                <div id="checkinfo">
+                    <p>${CartmealData.strMeal}</p>
+                    <div id="lowerInfo">
+                        <div id="price">
+                            <p id="priceAmount">NGN <span>1000</span>.00</p>
+                            <p id="shipping">Free Shipping</p>
+                        </div>
+                        <div id="quantity">
+                            <span id="minus" onclick="updateMealQuantity(this)">-</span>
+                            <span id="value">1</span>
+                            <span id="plus" onclick="updateMealQuantity(this)">+</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `
+        
     }
 }
 
 
 
 
+
 let updateMealQuantity = function (x) {
     // where x is the element that called this function
+    
+    let mealQuantity = document.querySelector("#value");
+
+    if (x.innerText === "+") {
+        mealQuantity.innerText++;
+        
+    } else if (x.innerText === "-") {
+        if (mealQuantity.innerText < 2) {
+            alert("Minimum meal is 1")
+        } else{
+            mealQuantity.innerText--;
+            
+
+        }
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+// invoking this function(s) whenever the cart page loads
+getCartedMealIdFromLS();
+
+
+
+
+/*
+
+
+let updateMealQuantity = function (x) {
+    // where x is the element that called this function
+    
+    let getMealQuantityValue = document.querySelector("#value");
+
     if (x.innerText === "+") {
         getMealQuantityValue.innerText++;
         getMealPrice.innerText = getMealQuantityValue.innerText * defaultMealPrice;
@@ -56,9 +115,21 @@ let updateMealQuantity = function (x) {
     }
 }
 
+
+
+let checkMealQuantityStatus = () => {
+    setInterval(checkCurrentMealQuantity, 100)
+ }
+
+
+
+
+ 
 // function to disable the minus button whenever meal quantity is 1
 // to avoid setting meal quantity to a negative integers or 0
 let checkCurrentMealQuantity = () => {
+    let getPlusMealQuantity = document.querySelector("#plus");
+    let getMinusMealQuantity = document.querySelector("#minus");
     
     if (getMealQuantityValue.innerText > 1) {
        getMinusMealQuantity.style.backgroundColor = "white"
@@ -67,19 +138,5 @@ let checkCurrentMealQuantity = () => {
      }
 }
 
- let checkMealQuantityStatus = () => {
-    setInterval(checkCurrentMealQuantity, 100)
- }
 
-
-
-
-
-
-
-
-
-
-// invoking this function(s) whenever the cart page loads
-// getCartedMealIdFromLS();
-checkMealQuantityStatus()
+*/
