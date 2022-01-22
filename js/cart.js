e9cd5f65d18f6becff312366db0bea96192077b6
Abubakri Sherif <75755419+saykeed@users.noreply.github.com>
2022-01-22 12:23:36 +0100

    Worked on the checkout modal and also animations

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,6 @@
 let getCartMealDiv = document.querySelector("#cartDiv");
 let getNumofCartedMeals = document.querySelector("#cartHeadTitle span");
+let getModal = document.querySelector("#modal");
 
 
 // function that get the number of carted meals and display at top of the cart page
@@ -49,7 +50,7 @@ let getCartedMeal = async function (id) {
                     <div id="lowerInfo">
                         <div id="price">
                             <p id="priceAmount">NGN <span>1000</span>.00</p>
-                            <p id="shipping">Free Shipping</p>
+                            <p class="shipping">Free Shipping</p>
                         </div>
                         <div class="quantity">
                             <span id="minus" onclick="updateMealQuantity(this, ${CartmealData.idMeal}, 1000)">-</span>
@@ -67,11 +68,11 @@ let getCartedMeal = async function (id) {
 
 
 
-let updateMealQuantity = function (x, y, p) {
+let updateMealQuantity = function (x, id, p) {
     //  x is the element that called this function
     // y is the meal id
     // p is the price of the meal
-    
+    y = String(id);
     let mealQuantity = document.querySelector("#a" + y + " #value");
     let mealPrice = document.querySelector("#a" + y + " #priceAmount span");
     
@@ -92,21 +93,28 @@ let updateMealQuantity = function (x, y, p) {
         delCheckedMealFromLS(y);
         updateNumOfCheckedMeals();
         minusMealPriceFromCheckoutAmount(mealTotalPrice);
+        
+        mealQuantity.innerText++;
+        minusBtn.style.backgroundColor = "white";
+        mealPrice.innerText = p * mealQuantity.innerText;
+        
+        getCheckBox.checked = true;
+        getcustomCheckBox.style.cssText = "background: linear-gradient(to bottom right, rgb(255, 0, 0), rgb(255, 89, 0), yellow); border:none;";
+        saveCheckedMealToLS(y);
+        updateNumOfCheckedMeals();
+        
+        let getupCheckedMealPrice = document.querySelector("#a" + y + " #priceAmount span");
+        let upmealTotalPrice = getupCheckedMealPrice.innerText;
+        
+        addMealPriceToCheckoutAmount(upmealTotalPrice);
+      } else {
+        mealQuantity.innerText++;
+        minusBtn.style.backgroundColor = "white";
+        mealPrice.innerText = p * mealQuantity.innerText;
+        
       }
       
-       mealQuantity.innerText++;
-       minusBtn.style.backgroundColor = "white";
-       mealPrice.innerText = p * mealQuantity.innerText;
        
-       getCheckBox.checked = true;
-       getcustomCheckBox.style.cssText = "background: linear-gradient(to bottom right, rgb(255, 0, 0), rgb(255, 89, 0), yellow); border:none;";
-         saveCheckedMealToLS(y);
-         updateNumOfCheckedMeals();
-         
-         let getupCheckedMealPrice = document.querySelector("#a" + y + " #priceAmount span");
-         let upmealTotalPrice = getupCheckedMealPrice.innerText;
-         
-         addMealPriceToCheckoutAmount(upmealTotalPrice);
          
     } else if (x.innerText === "-") {
         if (mealQuantity.innerText < 2) {
@@ -118,7 +126,7 @@ let updateMealQuantity = function (x, y, p) {
             delCheckedMealFromLS(y);
             updateNumOfCheckedMeals();
             minusMealPriceFromCheckoutAmount(mealTotalPrice);
-          }
+          
           
             mealQuantity.innerText--;
             if (mealQuantity.innerText === "1") {
@@ -134,6 +142,13 @@ let updateMealQuantity = function (x, y, p) {
             let upmealTotalPrice = getupCheckedMealPrice.innerText;
             
                 addMealPriceToCheckoutAmount(upmealTotalPrice);
+          } else {
+            mealQuantity.innerText--;
+            if (mealQuantity.innerText === "1") {
+              minusBtn.style.backgroundColor = "rgba(0,0,0,0.131)";
+            }
+            mealPrice.innerText = p * mealQuantity.innerText;
+          }
         }
     }
 }
@@ -201,11 +216,6 @@ let delFromLS = function () {
 }
 
 
-// function that runs whenever the checkout button is clicked
-let checkingOut = function () {
-  alert("Your total price is ...")
-}
-
 // function that add the checkedMealPrice to the checkout amount
 // take note of the uppercase letter P used in (ip and np) in this function
 let addMealPriceToCheckoutAmount = function (p) {
@@ -256,6 +266,134 @@ let delFromCart = () => {
 }
 
 
+// functions that handles the checkout modal
+
+// function that runs whenever the checkout button is clicked
+
+let checkingOut = async function () {
+  
+ let sAmount = Number(document.querySelector("#checkoutPrice span").innerText);
+ const sMeals = JSON.parse(localStorage.getItem('checkedMealId'));
+ 
+ 
+ if (!sMeals) {
+   alert("Nothing to checkout")
+ } else {
+ 
+ getModal.style.display = "block";
+
+ getModal.innerHTML = `
+          <div id="modHead">
+            <span onclick="closeModal()">&times;</span>
+            <h2>Checkout</h2>
+          </div>
+          <h4 id="modTab">Delivery</h4>
+          <p id="address">ADDRESS DETAILS</p>
+          <div id="location" class ="animate__animated animate__slideInUp animate__faster">
+            <h3>Would you like this meal delivered to your current location?</h3>
+            <div id="yesno">
+              <div onclick="getLocation()" id="yes">YES</div>
+              <div onclick="manuallyInputLocation()" id="no">NO</div>
+            </div>
+          </div>
+          
+          <p class="animate__animated animate__slideInRight animate__fast" id="shippinginfo">SHIPPING DETAILS</p>
+          <div id="shipping"></div>
+          
+          <div class="animate__animated animate__slideInUp animate__fast" id="total">
+            <p id="subtotal">Subtotal <span>NGN  <span>${sAmount}</span>.00</span></p>
+            <p id="shippingFee">Shipping <span>free</span></p>
+             <hr>
+            <p id="totalfee">Total <span>NGN <span>${sAmount}</span>.00</span></p>
+            <div onclick="loadPaymentTab()" id="propaymentBtn">PROCEED TO PAYMENT</div>
+          </div>
+ `;
+ 
+ let shiporder = document.querySelector("#shipping");
+ for (var i = 0; i < sMeals.length; i++) {
+   let mealQuantity = document.querySelector("#a" + sMeals[i] + " #value").innerText;
+   let smealUrl = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + sMeals[i];
+   let smeal = await fetch(smealUrl);
+   let smealRes = await smeal.json();
+   let ShipmealData = smealRes.meals[0];
+   
+   shiporder.innerHTML += `
+      <div id="eachorder">
+        <p id="package">PACKAGE ${i + 1} OF ${sMeals.length}</p>
+        <p id="order"> <span>${mealQuantity} x</span>${ShipmealData.strMeal}</p>
+        <p id="eta">delivered to you in less than 1hour</p>
+        <p id="prepare">Prepared by <img src="images/logo-small.png" alt=""></p>
+      </div>
+    `
+ };
+ 
+ 
+ setTimeout(updateScroll, 10);
+   }
+}
+
+// function that loads the modal payment tab
+let loadPaymentTab = function () {
+ let sAmount = Number(document.querySelector("#checkoutPrice span").innerText);
+ let getDeliveryAddress = document.querySelector("#inputAddress");
+ 
+ if (getDeliveryAddress == null || getDeliveryAddress.value == "") {
+   alert("Delivery address cannot be empty")
+ } else {
+  getModal.innerHTML = `
+         <div id="modHead">
+            <span onclick="closeModal()">&times;</span>
+            <h2>Checkout</h2>
+         </div> 
+         <h4 id="modTab">summary</h4> 
+         <p id ="address">YOUR ORDER</p> 
+         <div class ="animate__animated animate__slideInUp animate__fast" id="total">
+           <p id="subtotal">Subtotal <span>NGN  <span>${sAmount}</span>.00</span></p>
+            <p id="shippingFee">Shipping <span>free</span></p>
+            <hr>
+            <p id="totalfee">Total <span>NGN <span>${sAmount}</span>.00</span></p>
+           
+           </div>
+          <p id="address">YOUR ADDRESS</p>
+          <div class ="animate__animated animate__slideInUp animate__fast" id="userAddress">${getDeliveryAddress.value}</div>
+             
+       
+        <p id="address">PAYMENT METHOD</p>
+         <div id="paymentMethod">
+           Pay now. Stay safe. Go cashless with Paystack
+          </div>
+          <div id="propaymentBtn">CONFIRM</div>
+           <div onclick="checkingOut()" id="prevBtn">BACK TO DELIVERY TAB</div>
+  `
+  setTimeout(updateScroll, 10);
+ }
+}
+
+// function that scroll it back to top
+let updateScroll = () => {
+  getModal.scrollTop = 0;
+}
+
+// function that closes the modal whenever the close button is clicked
+let closeModal = function () {
+  
+  getModal.style.display = "none";
+}
+
+
+// function that get the users location
+let getLocation = function () {
+  alert("we cant get your current location now, kindly click on the NO button 👉 to input ur address manually")
+};
+
+let manuallyInputLocation = function () {
+  let getLocationDiv = document.querySelector("#location");
+  getLocationDiv.innerHTML += `
+  <input class ="animate__animated animate__slideInDown animate__fast" id="inputAddress" type="text" placeholder="addresss here"/>
+  `
+}
+
+
 
 
 
@@ -271,9 +409,28 @@ numOfCartedMeals();
 /*
 
 
+getModal.classList.add('animate__animated', 'animate__slideInUp');
 
 
 
 
+<div id="eachorder">
+              <p id="package">PACKAGE 1 OF 1</p>
+              <p id="order">
+                <span>1 x</span>
+                the name of the meal...
+              </p>
+               <p id="eta">delivered to you in less than 1hour</p>
+              <p id="prepare">Prepared by <img src="images/logo-small.png" alt=""></p>
+            </div>
+            <div id="eachorder">
+              <p id="package">PACKAGE 1 OF 1</p>
+              <p id="order">
+                <span>1 x</span>
+                the name of the meal...
+              </p>
+              <p id="eta">delivered to you in less than 1hour</p>
+              <p id="prepare">Prepared by <img src="images/logo-small.png" alt=""></p>
+            </div>
 
 */
